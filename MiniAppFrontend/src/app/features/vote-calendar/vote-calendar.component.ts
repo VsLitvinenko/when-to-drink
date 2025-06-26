a9cecb0051f3e33380939daa019abb051227766e
VsLitvinenko <alorsl@live.com>
2025-06-27 00:07:50 +0300

    start time less than end time validation

--- MiniAppFrontend/src/app/features/vote-calendar/vote-calendar.component.ts
+++ MiniAppFrontend/src/app/features/vote-calendar/vote-calendar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IonDatetime, IonChip, IonPopover } from '@ionic/angular/standalone';
+import { IonDatetime, IonChip, IonPopover, IonModal } from '@ionic/angular/standalone';
 import { SharedFeatureModule } from 'src/app/shared';
 import { IonDateSpecifyDirective } from './directives';
+import { format } from 'date-fns';
+
 
 @Component({
   selector: 'app-vote-calendar',
@@ -13,12 +15,21 @@ import { IonDateSpecifyDirective } from './directives';
     IonDatetime,
     IonChip,
     IonPopover,
+    IonModal,
   ]
 })
 export class VoteCalendarComponent  implements OnInit {
 
+  public readonly nowDate = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss")
+  
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  public isStartLessThanEnd(start: any, end: any): boolean {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    return startDate < endDate;
+  }
+  
 }
