28adc318745bb2a99a174590b5958b9539803397
VsLitvinenko <alorsl@live.com>
2025-06-30 00:17:26 +0300

    github pages build configuration

---
+++ docs/chunk-OAVWKKJC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-62TPDNO2.js";import"./chunk-7PWDY3UP.js";import"./chunk-JRYKI6H5.js";import"./chunk-SPMQWSIB.js";import"./chunk-C5RQ2IC2.js";import"./chunk-42C7ZIID.js";import"./chunk-2R6CW7ES.js";export{a as mdTransitionAnimation};
---
+++ docs/chunk-TO2Q4UEL.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-NKG53MNX.js";import"./chunk-7PWDY3UP.js";import"./chunk-JRYKI6H5.js";import"./chunk-SPMQWSIB.js";import"./chunk-C5RQ2IC2.js";import"./chunk-42C7ZIID.js";import"./chunk-2R6CW7ES.js";export{b as iosTransitionAnimation,a as shadow};
