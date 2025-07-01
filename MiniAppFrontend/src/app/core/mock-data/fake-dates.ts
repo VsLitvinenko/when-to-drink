367339ecbb374c65daab1197b7fa22f80b09a7ee
VsLitvinenko <alorsl@live.com>
2025-07-01 19:45:34 +0300

    small fixes

--- MiniAppFrontend/src/app/core/mock-data/fake-dates.ts
+++ MiniAppFrontend/src/app/core/mock-data/fake-dates.ts
@@ -4,7 +4,7 @@ import { fakeUsers } from './fake-users';
 
 export const fakeDates: ResultDate[] = [
   {
-    date: new Date(2025, 5, 7),
+    date: new Date(2025, 6, 7),
     type: VoteType.Ready,
     users: [
       fakeUsers[0],
@@ -13,7 +13,7 @@ export const fakeDates: ResultDate[] = [
     ],
   },
   {
-    date: new Date(2025, 5, 8),
+    date: new Date(2025, 6, 8),
     type: VoteType.Ready,
     users: [
       fakeUsers[0],
@@ -21,9 +21,9 @@ export const fakeDates: ResultDate[] = [
     ],
   },
   {
-    date: new Date(2025, 5, 10),
-    start: new Date(2025, 5, 10, 18, 30),
-    end: new Date(2025, 5, 10, 22, 0),
+    date: new Date(2025, 6, 10),
+    start: new Date(2025, 6, 10, 18, 30),
+    end: new Date(2025, 6, 10, 22, 0),
     type: VoteType.Time,
     users: [
       fakeUsers[0],
@@ -33,9 +33,9 @@ export const fakeDates: ResultDate[] = [
     ],
   },
   {
-    date: new Date(2025, 5, 13),
-    start: new Date(2025, 5, 10, 18, 30),
-    end: new Date(2025, 5, 10, 22, 0),
+    date: new Date(2025, 6, 13),
+    start: new Date(2025, 6, 10, 18, 30),
+    end: new Date(2025, 6, 10, 22, 0),
     type: VoteType.Maybe,
     users: [
       fakeUsers[0],
@@ -43,25 +43,25 @@ export const fakeDates: ResultDate[] = [
       fakeUsers[2],
       fakeUsers[3],
       fakeUsers[4],
-      fakeUsers[5],
+      fakeUsers[6],
     ],
   },
   {
-    date: new Date(2025, 5, 14),
+    date: new Date(2025, 6, 14),
     type: VoteType.Ready,
     users: [
       fakeUsers[0],
     ],
   },
   {
-    date: new Date(2025, 5, 15),
+    date: new Date(2025, 6, 15),
     type: VoteType.Maybe,
     users: [
       fakeUsers[0],
       fakeUsers[2],
       fakeUsers[3],
       fakeUsers[4],
-      fakeUsers[5],
+      fakeUsers[6],
     ],
   },
 ];
