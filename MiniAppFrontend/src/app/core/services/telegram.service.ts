fe32ead00ce04bfda4f09029c6a82a4bd6bc91fc
VsLitvinenko <alorsl@live.com>
2025-07-02 21:44:20 +0300

    telegram environment fix

--- MiniAppFrontend/src/app/core/services/telegram.service.ts
+++ MiniAppFrontend/src/app/core/services/telegram.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { filter, map, of, shareReplay } from 'rxjs';
+import { EMPTY, filter, map, Observable, of, shareReplay } from 'rxjs';
 import { AppColorScheme } from './theme.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TelegramService {
 
-  private readonly miniApp$ = of((window as any).Telegram).pipe(
-    filter(Boolean),
-    map((tg) => tg.WebApp),
-    shareReplay(1)
-  );
+  private readonly miniApp$ = this.getTelegramMiniApp();
 
   public readonly colorScheme$ = this.miniApp$.pipe(
     map((miniApp) => miniApp.colorScheme as AppColorScheme),
@@ -22,5 +19,15 @@ export class TelegramService {
 
   public initApp(): void {
     this.miniApp$.subscribe((val) => console.log(val));
+  }
+
+  private getTelegramMiniApp(): Observable<any> {
+    return !environment.isTelegramMiniApp
+      ? EMPTY
+      : of((window as any).Telegram).pipe(
+          filter(Boolean),
+          map((tg) => tg.WebApp),
+          shareReplay(1)
+        );
   }
 }
