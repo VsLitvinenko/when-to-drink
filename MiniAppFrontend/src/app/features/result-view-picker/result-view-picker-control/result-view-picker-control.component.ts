2605fb493faf23bd8aaa07972a6fe38fcedaeff6
VsLitvinenko <alorsl@live.com>
2025-07-03 18:23:58 +0300

    result view picker container component with animation

--- MiniAppFrontend/src/app/features/result-view-picker/result-view-picker-control/result-view-picker-control.component.ts
+++ MiniAppFrontend/src/app/features/result-view-picker/result-view-picker-control/result-view-picker-control.component.ts
@@ -1,19 +1,14 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { SharedFeatureModule } from 'src/app/shared';
-
-
-export enum ViewPick {
-  List = 'list',
-  Calendar = 'calendar',
-}
+import { ViewPick } from '../models/view-pick.enum';
 
 @Component({
-  selector: 'app-result-view-picker',
-  templateUrl: './result-view-picker.component.html',
-  styleUrls: ['./result-view-picker.component.scss'],
+  selector: 'app-result-view-picker-control',
+  templateUrl: './result-view-picker-control.component.html',
+  styleUrls: ['./result-view-picker-control.component.scss'],
   imports: [SharedFeatureModule],
 })
-export class ResultViewPickerComponent {
+export class ResultViewPickerControlComponent {
   public readonly ViewPick = ViewPick;
   public readonly value = signal(ViewPick.List);
 
--- MiniAppFrontend/src/app/pages/vote-event-page/vote-event-page.component.ts
+++ MiniAppFrontend/src/app/pages/vote-event-page/vote-event-page.component.ts
@@ -1,18 +1,12 @@
-import { Component, computed, OnInit, viewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView } from '@ionic/angular/standalone';
 import { EventMainInfoComponent } from 'src/app/features/event-main-info';
 import { ResultFiltersComponent } from 'src/app/features/result-filters';
 import { VoteCalendarComponent } from 'src/app/features/vote-calendar';
 import { PageCommonModule } from 'src/app/shared';
-import { ResultViewPickerComponent, ViewPick } from '../../features/result-view-picker';
+import { ResultViewPickerContainerComponent, ResultViewPickerControlComponent, ViewPick } from '../../features/result-view-picker';
 import { ResultViewCalendarComponent, ResultViewDirective, ResultViewListComponent } from 'src/app/features/result-view';
 import { VotePageLocalize } from './vote-event-page.localize';
-import { animate, group, query, style, transition, trigger } from '@angular/animations';
-
-const timing = '0.4s ease-in-out';
-const visible = { transform: 'translateX(0)' };
-const leaveEnd = { transform: `translateX({{ leaveEnd }}%)` };
-const enterStart = { transform: `translateX({{ enterStart }}%)` };
 
 @Component({
   selector: 'app-vote-event-page',
@@ -23,7 +17,8 @@ const enterStart = { transform: `translateX({{ enterStart }}%)` };
     VoteCalendarComponent,
     EventMainInfoComponent,
     ResultFiltersComponent,
-    ResultViewPickerComponent,
+    ResultViewPickerControlComponent,
+    ResultViewPickerContainerComponent,
     ResultViewListComponent,
     ResultViewCalendarComponent,
     ResultViewDirective,
@@ -35,36 +30,10 @@ const enterStart = { transform: `translateX({{ enterStart }}%)` };
   host: {
     class: 'app-page-inner',
   },
-  animations: [
-    trigger('openCloseResultView', [
-      transition('* => *',
-        [
-          group([
-            query(':enter', style(enterStart)),
-            query(':leave', [animate(timing, style(leaveEnd))], { optional: true }),
-            query(':enter', [animate(timing), style(visible)]),
-          ])
-        ],
-        { params: { leaveEnd: -120, enterStart: 120, } }
-      ),
-    ]),
-  ],
 })
 export class VoteEventPageComponent  implements OnInit {
   public readonly ViewPick = ViewPick;
   public readonly VotePageLocalize = VotePageLocalize;
-
-  private readonly viewPicker = viewChild<ResultViewPickerComponent>('viewPicker');
-  public readonly animationParams = computed(() => {
-    switch (this.viewPicker()?.value()) {
-      case ViewPick.List:
-        return { leaveEnd: 120, enterStart: -120 };
-      case ViewPick.Calendar:
-        return { leaveEnd: -120, enterStart: 120 };
-      default:
-        return {};
-    }
-  });
 
   constructor() { }
 
