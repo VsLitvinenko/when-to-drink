f2c98531b9eda1b3541a5a0407a18399fd41688c
VsLitvinenko <alorsl@live.com>
2025-07-03 18:01:37 +0300

    result view ng switch animation

--- MiniAppFrontend/src/main.ts
+++ MiniAppFrontend/src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 import localeRu from '@angular/common/locales/ru';
 
 registerLocaleData(localeRu)
@@ -14,6 +15,7 @@ bootstrapApplication(AppComponent, {
       provide: RouteReuseStrategy,
       useClass: IonicRouteStrategy,
     },
+    provideAnimations(),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
